7f4350bec01ea382a9211e9adeec2f00ccb9d221
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-22 13:42:39 -0800

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -1,35 +1,29 @@
 class SlingShot{
     constructor(bodyA, pointB){
-        var options = {
+        var slingshot_options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
         this.pointB = pointB
-        this.sling = Constraint.create(options);
+        this.sling = Matter.Constraint.create(slingshot_options);
         World.add(world, this.sling);
     }
 
     fly(){
         this.sling.bodyA = null;
     }
-
+    attach(body){
+        this.sling.bodyA = body;
+    }
     display(){
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(10);
-            stroke("#311708");
-            if(pointA.x<220){
-            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
-            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
-            image(this.sling3, pointA.x-30, pointA.y-10, 15,30);
-            }  else {
-                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
-                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
-                image(this.sling3, pointA.x+22.2567898, pointA.y-10, 15,30);
-            }
+            strokeWeight(3);
+            stroke("white");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
     
