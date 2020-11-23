7c9b02f509fb6dbbb324c5dd199a3785eac4ce97
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-23 08:54:04 -0800

    Update box.js

--- box.js
+++ box.js
@@ -1,7 +1,7 @@
 class Box  {
     constructor(x, y, width, height){
       var box_options ={
-          isStatic:true
+          isStatic:false
       }
       this.body = Bodies.rectangle(x,y,width,height,box_options);
       this.width = width;
